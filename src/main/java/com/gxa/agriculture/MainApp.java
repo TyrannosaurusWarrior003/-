f8d64fd913af4bbacf82add2b1da729f437ca629
modonghai <2042696261@qq.com>
2024-05-11 18:06:07 +0800

    第二次的版本

--- src/main/java/com/gxa/agriculture/MainApp.java
+++ src/main/java/com/gxa/agriculture/MainApp.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 @MapperScan(basePackages = "com/gxa/agriculture/mapper")
 public class MainApp {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class);
     }
